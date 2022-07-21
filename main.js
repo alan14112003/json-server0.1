027f2c41a2f3d8bf02ea18265f02c133dc6e56df
alan14112003 <sonnn.21it@gmil.com>
2022-07-21 09:14:24 +0700

    update1

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app     = express();
 
-app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 const PORT = process.env.PORT || 3000;
