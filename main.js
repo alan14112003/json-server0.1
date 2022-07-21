aa51131acdebb58f58fcef9998e6942749e2f9e6
alan14112003 <sonnn.21it@gmil.com>
2022-07-21 08:50:43 +0700

    update1

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ var app     = express();
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
